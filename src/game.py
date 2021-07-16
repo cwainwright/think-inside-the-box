0df7108389eca2ec86a1cb6dbd1fdb442ae0f25d
ConDar15 <jake.condar@gmail.com>
2021-07-17 00:07:31 +0100

    Updated the main scipt to allow testing of the different game sections

---
+++ src/game.py
@@ -0,0 +1,31 @@
+import threading
+from queue import Queue
+
+from blessed import Terminal
+
+FPS = 60
+
+
+class Game:
+    """The top level class for the game"""
+
+    def __init__(self, manager_cls: type):
+        self.manager_cls = manager_cls
+
+    def run(self) -> None:
+        """The run method for the game, handling the TUI"""
+        term = Terminal()
+        input_queue = Queue()
+
+        manager = self.manager_cls(input_queue, term)
+        manager_thread = threading.Thread(target=manager)
+        manager_thread.start()
+
+        with term.raw(), term.hidden_cursor(), term.location():
+            while manager_thread.is_alive():
+                inp = term.inkey(1 / FPS)
+
+                if inp != '':
+                    input_queue.put(inp)
+
+            print(term.normal + term.clear)
