9fe276a454aee6d755377df40ab889d6152b97d2
ConDar15 <jake.condar@gmail.com>
2021-07-17 13:23:14 +0100

    Made the over world game section Flake8 compliant

--- src/sections/over_world.py
+++ src/sections/over_world.py
@@ -1,5 +1,5 @@
 from queue import Queue
-from typing import Optional
+from typing import Callable, Optional
 
 from blessed import Terminal
 from blessed.keyboard import Keystroke
@@ -8,18 +8,24 @@ from src.sections.base import GameSection
 
 
 class OverWorld(GameSection):
+    """Over world game section"""
+
     def __init__(self, in_queue: Queue):
         super().__init__(in_queue)
         # Any other needed initialization on game start here
 
-    def handle_start(self, start_data: object):
+    def handle_start(self, start_data: object) -> bool:
+        """Inherit"""
         pass
 
-    def run_processing(self,  inp: Optional[Keystroke]) -> bool:
+    def run_processing(self, inp: Optional[Keystroke]) -> bool:
+        """Inherit"""
         pass
 
-    def run_rendering(self, terminal: Terminal):
+    def run_rendering(self, terminal: Terminal, echo: Callable[[str], None]) -> None:
+        """Inherit"""
         pass
 
     def handle_stop(self) -> object:
+        """Inherit"""
         pass
